bb4788aec017dd44f2eb3ea9a413342dcbd25344
Breno Nascimento Faria <brenonf@gmail.com>
2018-08-08 18:37:30 -0300

    Lanche - 1038

---
+++ 1038.py
@@ -0,0 +1,13 @@
+x = input().split()
+a, b = x
+if a == '1':
+    print('Total: R$ {:.2f}'.format(4.00*float(b)))
+if a == '2':
+    print('Total: R$ {:.2f}'.format(4.50*float(b)))
+if a == '3':
+    print('Total: R$ {:.2f}'.format(5.00*float(b)))
+if a == '4':
+    print('Total: R$ {:.2f}'.format(2.00*float(b)))
+if a == '5':
+ print('Total: R$ {:.2f}'.format(1.50*float(b)))
+
