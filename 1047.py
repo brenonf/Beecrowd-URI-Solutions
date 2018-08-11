116f4186a10d064f8014eed97faeebb665ddb171
Breno Nascimento Faria <brenonf@gmail.com>
2018-08-11 19:59:15 -0300

    Tempo de jogo com minutos - 1047

---
+++ 1047.py
@@ -0,0 +1,38 @@
+x = input().split()
+hi, mi, hf, mf = x
+
+hi = int(x[0])
+mi = int(x[1])
+hf = int(x[2])
+mf = int(x[3])
+
+if hi < hf:
+    h = hf - hi
+    if mi < mf:
+        m = mf - mi
+    if mi > mf:
+        h = h - 1
+        m = (60 - mi) + mf
+    if mi == mf:
+        m = 0
+if hi > hf:
+    h = (24 - hi) + hf
+    if mi < mf:
+        m = mf - mi
+    if mi > mf:
+        h = h - 1
+        m = (60 - mi) + mf
+    if mi == mf:
+        m = 0
+if hi == hf:
+    if mi < mf:
+        m = mf - mi
+        h = 0
+    if mi > mf:
+        m = (60 - mi) + mf
+        h = 23
+    if mi == mf:
+        h = 24
+        m = 0
+    
+print('O JOGO DUROU {} HORA(S) E {} MINUTO(S)'.format(h, m))
---
+++ 1048.py
@@ -0,0 +1,24 @@
+x = float(input())
+if x <= 400.00:
+    s = x * 1.15
+    r = s - x
+    p = 15
+if 400.01 <= x <= 800.00:
+    s = x * 1.12
+    r = s - x
+    p = 12
+if 800.01 <= x <= 1200.00:
+    s = x * 1.10
+    r = s - x
+    p = 10
+if 1200.01 <= x <= 2000.00:
+    s = x * 1.07
+    r = s - x
+    p = 7
+if  x > 2000.00:
+    s = x * 1.04
+    r = s - x
+    p = 4
+print('Novo salario: {:.2f}'.format(s))
+print('Reajuste ganho: {:.2f}'.format(r))
+print('Em percentual: {} %'.format(p))
