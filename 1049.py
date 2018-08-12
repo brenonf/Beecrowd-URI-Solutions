723b14445ca096464d6529477311fd6775dd7e72
Breno Nascimento Faria <brenonf@gmail.com>
2018-08-12 18:54:30 -0300

    Animal - 1049

---
+++ 1049.py
@@ -0,0 +1,29 @@
+x = input()
+y = input()
+z = input()
+
+if x == 'vertebrado' and y == 'ave' and z == 'carnivoro':
+    a = 'aguia'
+
+if x == 'vertebrado' and y == 'ave' and z == 'onivoro':
+    a = 'pomba'
+
+if x == 'vertebrado' and y == 'mamifero' and z == 'onivoro':
+    a = 'homem'
+
+if x == 'vertebrado' and y == 'mamifero' and z == 'herbivoro':
+    a = 'vaca'
+
+if x == 'invertebrado' and y == 'inseto' and z == 'hematofago':
+    a = 'pulga'
+
+if x == 'invertebrado' and y == 'inseto' and z == 'herbivoro':
+    a = 'lagarta'
+
+if x == 'invertebrado' and y == 'anelideo' and z == 'hematofago':
+    a = 'sanguessuga'
+
+if x == 'invertebrado' and y == 'anelideo' and z == 'onivoro':
+    a = 'minhoca'
+
+print(a)
