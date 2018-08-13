dc6facfaf43f0b46df35cca7360b9e177f0fb882
Breno Nascimento Faria <brenonf@gmail.com>
2018-08-13 19:55:39 -0300

    DDD - 1050

---
+++ 1050.py
@@ -0,0 +1,20 @@
+x = int(input())
+
+if x == 61:
+    print('Brasilia')
+if x == 71:
+    print('Salvador')
+if x == 11:
+    print('Sao Paulo')
+if x == 21:
+    print('Rio de Janeiro')
+if x == 32:
+    print('Juiz de Fora')
+if x == 19:
+    print('Campinas')
+if x == 27:
+    print('Vitoria')
+if x == 31:
+    print('Belo Horizonte')
+if x != 61 and  x != 71 and x != 11 and x != 21 and x != 32 and x != 19 and x != 27 and x != 31:
+    print('DDD nao cadastrado')
