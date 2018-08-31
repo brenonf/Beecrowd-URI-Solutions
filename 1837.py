0d6d8fa83fb73579fb977ba1dae055e520145aee
Breno Nascimento Faria <brenonf@gmail.com>
2018-08-31 15:10:03 -0300

    Prefácio (ainda solucionado) - 1837

---
+++ 1837.py
@@ -0,0 +1,13 @@
+a,b =map(int,input().split())
+m=max(abs(a),abs(b))
+
+for q in range(-m,m+1):
+    for r in range(-b,b+1):
+        if a == b * q + r:
+            quo = q
+            res = r
+            break
+print(quo,res)
+
+
+    
